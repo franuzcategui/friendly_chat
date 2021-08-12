15e91e79934a0ecd4273df2bc358ddd469ef99b3
franuzcategui <45088588+franuzcategui@users.noreply.github.com>
2021-08-11 18:26:33 -0600

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,42 @@ class FriendlyChatApp extends StatelessWidget {
 
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+
+  Widget _buildTextComposer() {
+    return  Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(
+            hintText: 'Send a message'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Friendly Chat')
-      ),
+      appBar: AppBar(title: const Text('Friendly Chat')),
+      body: _buildTextComposer(),
     );
   }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+
 }
 
 
